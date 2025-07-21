cbec9c94b6a28dafe5de8c6c80d2920eaf75a72d
Shlomi <shlomi10@gmail.com>
2025-07-21 15:41:47 +0300

    Save files to s3

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from invoice_reader import analyze_invoice_url
 from fastapi import FastAPI, HTTPException, Query
 from db import *
+from invoice_reader import save_invoice_to_db
+from fastapi import File, UploadFile
+from aws_file_utils import upload_and_get_presigned_url
+from invoice_reader import analyze_invoice_url
 
 '''
 FastAPI app with two endpoints:
@@ -26,7 +30,6 @@ def read_invoice_by_url(url: str = Query(..., description="Direct image URL")):
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-from invoice_reader import save_invoice_to_db
 @app.get("/read-invoice-by-url")
 def read_invoice_by_url(url: str = Query(..., description="Direct image URL")):
     try:
@@ -35,4 +38,16 @@ def read_invoice_by_url(url: str = Query(..., description="Direct image URL")):
         return {"status": "saved", "invoice": data}
     except Exception as e:
         raise HTTPException(status_code=400, detail=str(e))
+
+@app.post("/upload-invoice-file")
+def upload_invoice_file(file: UploadFile = File(...)):
+    try:
+        file_bytes = file.file.read()
+        key, url = upload_and_get_presigned_url(file_bytes, file.filename, file.content_type)
+        print (f"Presigned s3 url: {url}")
+        data = analyze_invoice_url(url)
+        save_invoice_to_db(data)
+        return {"status": "success", "invoice": data}
+    except Exception as e:
+        raise HTTPException(status_code=500, detail=str(e))
 
